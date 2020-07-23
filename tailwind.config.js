dc693ef435024a7aaeb2443b6968f996991321a7
BJ0815 <jtt11220@gmail.com>
2020-07-23 10:32:45 +0800

    ✨ [fix] 調整config/add postcss-import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
+// tailwind.config.js
+// const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   purge: {
-    enabled: true,
     content: ["./src/**/*.html"],
   },
   theme: {
-    extend: {},
+    extend: {
+      lineHeight: {
+        '20': '5rem'
+      },
+      height: {
+        'c-562': '562px',
+        'c-1292': '1292px',
+        'c-1241': '1241px'
+      },
+      inset: {
+        '0': 0,
+        '35': '35%',
+        '1/2': '50%',
+      }
+    },
   },
   variants: {},
   plugins: [],
